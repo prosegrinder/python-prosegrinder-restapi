037b21f373e52d3ead9915a27114ba9332b56fb6
David L. Day <dday376@gmail.com>
2021-08-17 22:09:49 -0400

    added get word

--- prosegrinder-restapi/__init__.py
+++ prosegrinder-restapi/__init__.py
@@ -1,15 +1,17 @@
-import os
+import os, json
 
 from flask import Flask
+from prosegrinder import Dictionary
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'prosegrinder.sqlite'),
-    )
+    # app.config.from_mapping(
+    #     SECRET_KEY='dev',
+    #     DATABASE=os.path.join(app.instance_path, 'prosegrinder.sqlite'),
+    # )
+    dictionary = Dictionary()
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -25,8 +27,29 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/hello')
+    @app.route('/')
     def hello():
         return 'Hello, World!'
+
+    @app.route('/word/<word>')
+    def word(word):
+        w = dictionary.get_word(word)
+        d = {
+            "text": w.text,
+            "phones": w.phones,
+            "normalized_phones": w.normalized_phones,
+            "phone_count": w.phone_count,
+            "syllable_count": w.syllable_count,
+            "character_count": w.character_count,
+            "is_dictionary_word": w.is_dictionary_word,
+            "is_numeric_word": w.is_numeric,
+            "is_complex_word": w.is_complex_word,
+            "is_pov_word": w.is_pov_word,
+            "is_first_person_word": w.is_first_person_word,
+            "is_second_person_word": w.is_second_person_word,
+            "is_third_person_word": w.is_third_person_word,
+            "pov": w.pov,
+        }
+        return json.dumps(d)
 
     return app
